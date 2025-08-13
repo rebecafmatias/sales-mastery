185a3ade9fea78df169ce42ebba35b2d7ebe1bb7
Rebeca Feitosa Matias <rebecafeitosamatias@gmail.com>
2025-08-12 21:12:07 -0300

    teste

---
+++ src/sales_file_analysis.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Challenge 1 – Reading and inspection
+# Read each of the three CSV files using pandas (sales.csv, products.csv, customers.csv).
+# Display the first 5 rows of each DataFrame.
+# Show the total number of rows in each file.
+
